98288a1d5cea48ffb468ad6956c2622e1fc93122
ChristianMBoyd <Boyd.Christian.M@gmail.com>
2023-12-28 14:57:42 -0500

    Implemented tests of the Coordinate and Position class

--- UnitTests/CoordinateTests.cpp
+++ UnitTests/CoordinateTests.cpp
@@ -3,12 +3,18 @@
 #include "../Position.h"
 #include "RandomGenerator.h"
 
-namespace CoordinateTests { // local functions and names
+/*
+Currently, the coordinate operations are being tested via the inherited class Position. Since 
+Position only extends named accessors, the only distinction is implicitly testing these accessors.
+*/
+
+namespace { // local functions and names
 
 	// initialize random floats
 	const int minPosition = -10000;
 	const int maxPosition = 10000;
 	RandomGenerator positionGenerator(minPosition, maxPosition);
+
 	TEST(CoordinateTests, TestConstructor) {
 		const float time = std::abs(positionGenerator.randomFloat());
 		const float x = positionGenerator.randomFloat();
@@ -17,6 +23,86 @@ namespace CoordinateTests { // local functions and names
 		Position position(time, x, y, z);
 		EXPECT_FLOAT_EQ(position.timeInSeconds(), time) << "assigned " 
 			<< time << " returned " << position.timeInSeconds();
+		EXPECT_FLOAT_EQ(position.xInMeters(), x) << "assigned "
+			<< x << " returned " << position.xInMeters();
+		EXPECT_FLOAT_EQ(position.yInMeters(), y) << "assigned "
+			<< y << " returned " << position.yInMeters();
+		EXPECT_FLOAT_EQ(position.zInMeters(), z) << "assigned "
+			<< z << " returned " << position.zInMeters();
 	}
 
+	// at this point, the constructor passed its tests
+	Position randomPosition() {
+		float time = std::abs(positionGenerator.randomFloat());
+		float x = positionGenerator.randomFloat();
+		float y = positionGenerator.randomFloat();
+		float z = positionGenerator.randomFloat();
+		return Position(time, x, y, z);
+	}
+
+	TEST(CoordinateTests, TestAddition) {
+		const Position firstPosition = randomPosition();
+		const Position secondPosition = randomPosition();
+		const Position sumPosition = firstPosition + secondPosition;
+		float sum = firstPosition.timeInSeconds() + secondPosition.timeInSeconds();
+		EXPECT_FLOAT_EQ(sumPosition.timeInSeconds(), sum) << "Evaluated "
+			<< firstPosition.timeInSeconds() << " - " << secondPosition.timeInSeconds()
+			<< " as " << sumPosition.timeInSeconds();
+		sum = firstPosition.xInMeters() + secondPosition.xInMeters();
+		EXPECT_FLOAT_EQ(sumPosition.xInMeters(), sum) << "Evaluated "
+			<< firstPosition.xInMeters() << " - " << secondPosition.xInMeters()
+			<< " as " << sumPosition.xInMeters();
+		sum = firstPosition.yInMeters() + secondPosition.yInMeters();
+		EXPECT_FLOAT_EQ(sumPosition.yInMeters(), sum) << "Evaluated "
+			<< firstPosition.yInMeters() << " - " << secondPosition.yInMeters()
+			<< " as " << sumPosition.yInMeters();
+		sum = firstPosition.zInMeters() + secondPosition.zInMeters();
+		EXPECT_FLOAT_EQ(sumPosition.zInMeters(), sum) << "Evaluated "
+			<< firstPosition.zInMeters() << " - " << secondPosition.zInMeters()
+			<< " as " << sumPosition.zInMeters();
+	}
+
+	TEST(CoordinateTests, TestSubtraction) {
+		const Position firstPosition = randomPosition();
+		const Position secondPosition = randomPosition();
+		const Position differencePosition = firstPosition - secondPosition;
+		float difference = firstPosition.timeInSeconds() - secondPosition.timeInSeconds();
+		EXPECT_FLOAT_EQ(differencePosition.timeInSeconds(), difference) << "Evaluated "
+			<< firstPosition.timeInSeconds() << " - " << secondPosition.timeInSeconds()
+			<< " as " << differencePosition.timeInSeconds();
+		difference = firstPosition.xInMeters() - secondPosition.xInMeters();
+		EXPECT_FLOAT_EQ(differencePosition.xInMeters(), difference) << "Evaluated "
+			<< firstPosition.xInMeters() << " - " << secondPosition.xInMeters()
+			<< " as " << differencePosition.xInMeters();
+		difference = firstPosition.yInMeters() - secondPosition.yInMeters();
+		EXPECT_FLOAT_EQ(differencePosition.yInMeters(), difference) << "Evaluated "
+			<< firstPosition.yInMeters() << " - " << secondPosition.yInMeters()
+			<< " as " << differencePosition.yInMeters();
+		difference = firstPosition.zInMeters() - secondPosition.zInMeters();
+		EXPECT_FLOAT_EQ(differencePosition.zInMeters(), difference) << "Evaluated "
+			<< firstPosition.zInMeters() << " - " << secondPosition.zInMeters()
+			<< " as " << differencePosition.zInMeters();
+	}
+
+	TEST(CoordinateTests, TestScalarMultiplication) {
+		const Position position = randomPosition();
+		const float scaleFactor = positionGenerator.randomFloat();
+		const Position scaledPosition = scaleFactor * position;
+		float product = scaleFactor * position.timeInSeconds();
+		EXPECT_FLOAT_EQ(scaledPosition.timeInSeconds(), product) << "Evaluated "
+			<< scaleFactor << " * " << position.timeInSeconds() << " as "
+			<< scaledPosition.timeInSeconds();
+		product = scaleFactor * position.xInMeters();
+		EXPECT_FLOAT_EQ(scaledPosition.xInMeters(), product) << "Evaluated "
+			<< scaleFactor << " * " << position.xInMeters() << " as "
+			<< scaledPosition.xInMeters();
+		product = scaleFactor * position.yInMeters();
+		EXPECT_FLOAT_EQ(scaledPosition.yInMeters(), product) << "Evaluated "
+			<< scaleFactor << " * " << position.yInMeters() << " as "
+			<< scaledPosition.yInMeters();
+		product = scaleFactor * position.zInMeters();
+		EXPECT_FLOAT_EQ(scaledPosition.zInMeters(), product) << "Evaluated "
+			<< scaleFactor << " * " << position.zInMeters() << " as "
+			<< scaledPosition.zInMeters();
+	}
 }
